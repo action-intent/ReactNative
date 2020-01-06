7a4b06335971664cc1759f849726c54355b5da15
action-intent <actionintent85@gmail.com>
2020-01-06 19:41:34 +0900

    Animation example

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './navigator/CitiesApp';
+import App from './animation/ParallelApp';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
