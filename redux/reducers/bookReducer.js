409356fd239a6b08b005712e77a23bc7416c68f0
action-intent <actionintent85@gmail.com>
2020-01-10 00:12:25 +0900

    redux example

---
+++ redux/reducers/bookReducer.js
@@ -0,0 +1,24 @@
+import { ADD_BOOK, REMOVE_BOOK } from '../actions'
+import uuidV4 from 'uuid/v4'
+
+const initialState = {
+    books: [{ name: 'East of Eden', author: 'Jhon', id: uuidV4()}]
+}
+
+const bookReducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_BOOK:
+            return{
+                books:[...state.books, action.book]
+            }
+        case REMOVE_BOOK:
+            const index = state.books.findIndex(book => book.id === action.book.id)
+            return {
+                books: [...state.books.slice(0, index), ...state.books.slice(index+1)]
+            }
+        default:
+            return state
+    }
+}
+
+export default bookReducer
